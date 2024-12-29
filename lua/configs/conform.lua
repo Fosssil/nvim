029ca8e4b5b6bb4be9f02fa848b8c37fc848da0e
Fosssil <dahiyabadshah.fossil@gmail.com>
2024-12-29 12:47:32 +0530

    Major custom changes

---
+++ lua/configs/conform.lua
@@ -0,0 +1,22 @@
+local options = {
+  formatters_by_ft = {
+    assembly = { "asmfmt" },
+    css = { "prettier" },
+    c = { "clang_format" },
+    html = { "prettier" },
+    lua = { "stylua" },
+    python = { "isort", "black" },
+    rust = { "rustfmt" },
+    shell = { "shfmt" },
+    terraform = { "terraform_fmt" },
+  },
+
+  -- format_on_save = {
+  --   --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
+  vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, { desc = "format documet" }),
+}
+
+return options
---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,36 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+require("lspconfig").html.setup {}
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").ansiblels.setup {}
+require("lspconfig").bashls.setup {}
+require("lspconfig").pylsp.setup {}
+require("lspconfig").yamlls.setup {}
+require("lspconfig").clangd.setup {}
+require("lspconfig").terraformls.setup {}
+require("lspconfig").rust_analyzer.setup {}
+-- require("lspconfig").harper_ls.setup {}
+-- require("lspconfig").asm_lsp.setup {}
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }

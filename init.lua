9686dbd55a0c67686b60481372d64e1e1b2a0d0b
Fosssil <dahiyabadshah.fossil@gmail.com>
2024-12-29 12:55:47 +0530

    Added extra vim options

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
+vim.cmd("set showmatch")
+vim.cmd("set relativenumber")
+vim.cmd("set ignorecase")
+vim.cmd("set hlsearch")
+vim.cmd("set incsearch")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
+vim.cmd("set number")
+vim.cmd("set wildmode=longest,list")
+vim.cmd("set mouse=")
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set cursorline")
+vim.cmd("set ttyfast")
+vim.cmd("syntax on")
+vim.cmd("filetype plugin on")
+vim.cmd("filetype plugin indent on")
+
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 

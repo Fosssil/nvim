d3697945de2ac8a10c13db2acac0816aa4efe418
Fosssil <dahiyabadshah.fossil@gmail.com>
2025-07-30 02:51:23 +0530

    30-Jul-2025 02:51 AM

--- lua/plugins/autoinstaller.lua
+++ lua/plugins/autoinstaller.lua
@@ -1,35 +1,38 @@
 return {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                -- LSP servers
-                "bashls",
-                "cssls",
-                "html",
-                "lua_ls",
-                "jsonls",
-                "hyprls",
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = { "williamboman/mason.nvim" },
+	config = function()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				-- LSP servers
+				"bashls",
+				"cssls",
+				"html",
+				"lua_ls",
+				"jsonls",
+				"hyprls",
+				"gopls",
 
-                -- Formatters
-                "stylua",
-                "prettier",
-                "shfmt",
+				-- Formatters
+				"stylua",
+				"prettier",
+				"shfmt",
+				"gofumpt",
 
-                -- Linters
-                "luacheck",
-                "shellcheck",
-                "stylelint",
-                "jsonlint",
-                "htmlhint",
-            },
-            auto_update = true,
-            run_on_start = true,
-            start_delay = 3000, -- Delay (ms) to reduce startup load
-            integrations = {
-                ['mason-lspconfig'] = true,
-            },
-        })
-    end,
+				-- Linters
+				"luacheck",
+				"shellcheck",
+				"stylelint",
+				"jsonlint",
+				"htmlhint",
+				"golangci-lint",
+			},
+			auto_update = true,
+			run_on_start = true,
+			start_delay = 3000, -- Delay (ms) to reduce startup load
+			integrations = {
+				["mason-lspconfig"] = true,
+			},
+		})
+	end,
 }

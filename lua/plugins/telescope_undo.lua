785852c1248e348acd03dba95e5060b4c048c2a7
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-08-18 00:04:09 +0530

    Reconfigured telescope-undo

---
+++ lua/plugins/telescope_undo.lua
@@ -0,0 +1,54 @@
+-- ~/.config/nvim/lua/plugins/telescope_undo.lua
+
+return {
+	"debugloop/telescope-undo.nvim",
+
+	dependencies = {
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+		},
+	},
+
+	keys = {
+		{
+			"<leader>u",
+			"<cmd>Telescope undo<CR>",
+			desc = "Undo History",
+		},
+	},
+
+	opts = {
+		extensions = {
+			undo = {
+				use_delta = false,
+				side_by_side = false,
+
+				layout_strategy = "horizontal",
+
+				layout_config = {
+					height = 0.9,
+					width = 0.9,
+					preview_width = 0.55,
+				},
+
+				vim_diff_opts = {
+					ctxlen = 3,
+				},
+
+				entry_format = "state #$ID, $STAT, $TIME",
+				time_format = "%H:%M:%S",
+				saved_only = false,
+			},
+		},
+	},
+
+	config = function(_, opts)
+		local telescope = require("telescope")
+
+		telescope.setup(opts)
+		telescope.load_extension("undo")
+	end,
+}

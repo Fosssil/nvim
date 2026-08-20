0bcbe36ce95ee7652b76a6f68f7c11c55c481559
Fosssil <dahiyabadshah.fossil@gmail.com>
2026-08-21 02:51:17 +0530

    Changed annoying option

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -37,8 +37,8 @@ return {
 				current_line_blame = true,
 
 				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol",
+					virt_text = false,
+					virt_text_pos = "right_align",
 					delay = 500,
 					ignore_whitespace = true,
 				},
